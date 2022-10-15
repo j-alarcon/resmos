2f51e33fd1cf27ca974d1b92d4839d79c91dce38
yarduon <yard.campbs@gmail.com>
2022-10-15 20:05:02 +0100

    Update version data

--- json/languages.js
+++ json/languages.js
@@ -1,6 +1,6 @@
 // https://yarduon.com
 export default {
-  es: ["FÁCIL", "NORMAL", "DIFÍCIL", "Tú", "Versión 1.1", "02/09/2022"],
-  fr: ["FACILE", "NORMAL", "DIFFICILE", "Vous", "Version 1.1", "02/09/2022"],
-  de: ["LEICHT", "NORMAL", "SCHWIERIG", "Sie", "Version 1.1", "02/09/2022"],
+  es: ["FÁCIL", "NORMAL", "DIFÍCIL", "Tú", "Versión 1.1", "19/10/2022"],
+  fr: ["FACILE", "NORMAL", "DIFFICILE", "Vous", "Version 1.1", "19/10/2022"],
+  de: ["LEICHT", "NORMAL", "SCHWIERIG", "Sie", "Version 1.1", "2022/10/19"],
 };
