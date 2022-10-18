9da9dab67a50da80a4d878f86704dde4fab773ac
yarduon <yard.campbs@gmail.com>
2022-10-18 13:11:38 +0100

    Update portraits and data version

--- json/languages.js
+++ json/languages.js
@@ -1,6 +1,6 @@
 // https://yarduon.com
 export default {
-  es: ["FÁCIL", "NORMAL", "DIFÍCIL", "Tú", "Versión 1.2", "19/10/2022"],
-  fr: ["FACILE", "NORMAL", "DIFFICILE", "Vous", "Version 1.2", "19/10/2022"],
-  de: ["LEICHT", "NORMAL", "SCHWIERIG", "Sie", "Version 1.2", "2022/10/19"],
+  es: ["FÁCIL", "NORMAL", "DIFÍCIL", "Tú", "Versión 1.3", "20/10/2022"],
+  fr: ["FACILE", "NORMAL", "DIFFICILE", "Vous", "Version 1.3", "20/10/2022"],
+  de: ["LEICHT", "NORMAL", "SCHWIERIG", "Sie", "Version 1.3", "2022/10/20"],
 };
