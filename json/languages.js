40aec3f2cc8488619ccdc849dc044c3c9b4f7350
yarduon <yard.campbs@gmail.com>
2023-01-30 12:39:30 +0000

    Update workbox

--- json/languages.js
+++ json/languages.js
@@ -1,6 +1,6 @@
 // https://yarduon.com
 export default {
-  es: ["FÁCIL", "NORMAL", "DIFÍCIL", "Tú", "Versión 1.3", "20/10/2022"],
-  fr: ["FACILE", "NORMAL", "DIFFICILE", "Vous", "Version 1.3", "20/10/2022"],
-  de: ["LEICHT", "NORMAL", "SCHWIERIG", "Sie", "Version 1.3", "2022/10/20"],
+  es: ["FÁCIL", "NORMAL", "DIFÍCIL", "Tú", "Versión 1.4", "30/01/2023"],
+  fr: ["FACILE", "NORMAL", "DIFFICILE", "Vous", "Version 1.4", "30/01/2023"],
+  de: ["LEICHT", "NORMAL", "SCHWIERIG", "Sie", "Version 1.4", "2023/01/30"],
 };
--- sw.js
+++ sw.js
@@ -23,8 +23,8 @@ if (!self.define) {
     if (i[c]) return;
     let r = {};
     const o = (e) => s(e, c),
-      f = { module: { uri: c }, exports: r, require: o };
-    i[c] = Promise.all(n.map((e) => f[e] || o(e))).then((e) => (a(...e), r));
+      d = { module: { uri: c }, exports: r, require: o };
+    i[c] = Promise.all(n.map((e) => d[e] || o(e))).then((e) => (a(...e), r));
   };
 }
 define(["./workbox-7e688afb"], function (e) {
@@ -50,7 +50,7 @@ define(["./workbox-7e688afb"], function (e) {
         },
         {
           url: "css/animation.css",
-          revision: "4cbf23425b05f1ef26b48dd35f9f0b63",
+          revision: "9b9bc3414cb60b0ab765ec54c604532e",
         },
         { url: "css/fonts.css", revision: "871492272a48d9a3f675be608f7756aa" },
         { url: "css/main.css", revision: "3e6c8441bfb497dbaec8b8b7493d421c" },
@@ -133,52 +133,52 @@ define(["./workbox-7e688afb"], function (e) {
           revision: "a341d63b43208e9dff90b64c4888f18e",
         },
         {
-          url: "img/logos/white_penguin.png",
-          revision: "4af174c330d334424b2e206cc9dc4c62",
+          url: "img/logos/white_penguin_128x128.png",
+          revision: "521069a5755644473fe94126ee966ab5",
         },
         {
           url: "img/portraits/1.png",
-          revision: "c28f31e5195ba9bface14260ce7d0615",
+          revision: "bb13a59cc81c7172e0d35eea41a6f518",
         },
         {
           url: "img/portraits/10.png",
-          revision: "62dbcfe7bfe0ca1cf5f21bd7859d1476",
+          revision: "319132eef9dab618f9272fa166889287",
         },
         {
           url: "img/portraits/2.png",
-          revision: "ba9fc65dd8a29930d2de6c0afc6bc692",
+          revision: "97d86f3e1ae16107850414355193090a",
         },
         {
           url: "img/portraits/3.png",
-          revision: "1514f27302047163acbe031bb59cef89",
+          revision: "ed2262b41386d290e3422a09d9e2d5dc",
         },
         {
           url: "img/portraits/4.png",
-          revision: "59270afd2c14514774a5a7f2be780509",
+          revision: "3c8cdd010ffb71a42c9102b99e064f61",
         },
         {
           url: "img/portraits/5.png",
-          revision: "5af46ef6a511d9497e26902ba2cfcb28",
+          revision: "23ec4d2d166c597fe208d87bfd01355f",
         },
         {
           url: "img/portraits/6.png",
-          revision: "292c7b8e81910d929eb8843fe87cffee",
+          revision: "94af8ed9898e3a25c2fb5e32c33f3ecb",
         },
         {
           url: "img/portraits/7.png",
-          revision: "3a1a62737701b57ca2e758929e5412ad",
+          revision: "ab38c66c388c98f68f0b139d56f6ceab",
         },
         {
           url: "img/portraits/8.png",
-          revision: "5b3cae604e22ecdbaf7a8e8bd2c19e61",
+          revision: "e9966655dbde1cd442b9807d20724590",
         },
         {
           url: "img/portraits/9.png",
-          revision: "0eb0671939cff366c2579094c9d5fb27",
+          revision: "e7b489f85addab70591ce7f72a05f9b9",
         },
         {
           url: "img/portraits/default.png",
-          revision: "17f5fe97af43746dc8411b1dc942cfed",
+          revision: "92dbcc02d7a2c33a953583fcc055a066",
         },
         {
           url: "img/pwa/badges/android_en.png",
@@ -238,141 +238,141 @@ define(["./workbox-7e688afb"], function (e) {
         },
         {
           url: "img/pwa/screenshots/english/computer_end_en.png",
-          revision: "ad7b68c0b6988b4e38fd4bee5b27c782",
+          revision: "4455b6d99b664048a14270a4d93d6e44",
         },
         {
           url: "img/pwa/screenshots/english/computer_half_life_en.png",
-          revision: "4ef2aa32d4b60f9102b6080b415070b7",
+          revision: "31bb6d5c9d386f2ae62d3d4de7f393ee",
         },
         {
           url: "img/pwa/screenshots/english/computer_low_life_en.png",
-          revision: "7057af236c6a2c8d24248160d8ed2222",
+          revision: "c8a337d03e5888e1130344063c779d4e",
         },
         {
           url: "img/pwa/screenshots/english/computer_start_en.png",
-          revision: "715a91073b6d93f08b91fc1087c217d2",
+          revision: "c6fe3872b19668ee7c6cc170ef24862f",
         },
         {
           url: "img/pwa/screenshots/english/mobile_end_en.png",
-          revision: "22d08e1155862879fc5fe72feea50818",
+          revision: "a8c0d73001fb83721b8d323a459f8bf1",
         },
         {
           url: "img/pwa/screenshots/english/mobile_half_life_en.png",
-          revision: "bd91440259b729f979527bdda9c5d3a7",
+          revision: "9421ae1a1865712bd0f05c7671ed8b44",
         },
         {
           url: "img/pwa/screenshots/english/mobile_low_life_en.png",
-          revision: "4e4947c00abcf6423c826c9492cd6102",
+          revision: "7643801bc30a5e229c78f774eefe05aa",
         },
         {
           url: "img/pwa/screenshots/english/mobile_start_en.png",
-          revision: "c21840ab2263c8850fc3155d7af11a85",
+          revision: "16dd07c657d0e63ce6c3901da65dbfaa",
         },
         {
           url: "img/pwa/screenshots/english/tablet_10inch_end_en.png",
-          revision: "f6f5c5c695315deb5e1ede1671632e9b",
+          revision: "25f89b0fb93167dfd3cca9e9a93d0d6c",
         },
         {
           url: "img/pwa/screenshots/english/tablet_10inch_half_life_en.png",
-          revision: "660ac635eec0eb497c31f826b35d6f74",
+          revision: "c3d6ebc5b460a5f9236bafa387c308aa",
         },
         {
           url: "img/pwa/screenshots/english/tablet_10inch_low_life_en.png",
-          revision: "348f79a5f74bb5351e049900ed2cf02b",
+          revision: "28f123c27f731e5451fd4cbbccb77dd4",
         },
         {
           url: "img/pwa/screenshots/english/tablet_10inch_start_en.png",
-          revision: "f40377122d29e7a79555b530dcb3fdad",
+          revision: "91e0dd9cb4f4987dc285b89ce48beece",
         },
         {
           url: "img/pwa/screenshots/english/tablet_7inch_end_en.png",
-          revision: "46c250d33e6fee06e1c798a3007a2605",
+          revision: "2c020c2bf9074831e637e9805d2d79fc",
         },
         {
           url: "img/pwa/screenshots/english/tablet_7inch_half_life_en.png",
-          revision: "40bee1420840c0bad886ed7aa74ad5e8",
+          revision: "e4bf5627b51131454b1e32b3d834c49b",
         },
         {
           url: "img/pwa/screenshots/english/tablet_7inch_low_life_en.png",
-          revision: "2f7b35ac4fae6e8a2251328409e71548",
+          revision: "8a2bb8a8abc62b7082242794f7092566",
         },
         {
           url: "img/pwa/screenshots/english/tablet_7inch_start_en.png",
-          revision: "fd45712c991eb8bf5e1690ffbed14e81",
+          revision: "7c10156cd52fdb3b581dcd08781ef959",
         },
         {
           url: "img/pwa/screenshots/spanish/computer_end_spa.png",
-          revision: "fa5d3c0c582d151fd1a0b5a31f725bfa",
+          revision: "7ab65f5bfd8d2be3e4b6db92d1f90a92",
         },
         {
           url: "img/pwa/screenshots/spanish/computer_half_life_spa.png",
-          revision: "d01316012f5c89c02b2e3093d98b6662",
+          revision: "489ca765464559e2ead8ea8e5d82e56e",
         },
         {
           url: "img/pwa/screenshots/spanish/computer_low_life_spa.png",
-          revision: "19ab6e7da2eaef0872050a806d5424ab",
+          revision: "e4dd19d2a64e088a7284199c4175528e",
         },
         {
           url: "img/pwa/screenshots/spanish/computer_start_spa.png",
-          revision: "4a70eae5113d9cda0bb1b033f21a3e4b",
+          revision: "76363d43fe0ee0d4669d48c297c4dce7",
         },
         {
           url: "img/pwa/screenshots/spanish/mobile_end_spa.png",
-          revision: "d0f55df60ababde90e9a47d8cec2df17",
+          revision: "69f89d48c333579c79e61d21d81cddee",
         },
         {
           url: "img/pwa/screenshots/spanish/mobile_half_life_spa.png",
-          revision: "161e894852ead4c7e0a5327b94b7bb19",
+          revision: "056a4b157b84b6599b1baee449db8935",
         },
         {
           url: "img/pwa/screenshots/spanish/mobile_low_life_spa.png",
-          revision: "8120fb0e88cec4185752052b464a64a2",
+          revision: "a85a3a6c7d6c3f76100d72e8795b4e96",
         },
         {
           url: "img/pwa/screenshots/spanish/mobile_start_spa.png",
-          revision: "5732eab13ec1981cedebd319edd27d0c",
+          revision: "78a70cf30c13d8e0d3b632fba682128b",
         },
         {
           url: "img/pwa/screenshots/spanish/tablet_10inch_end_spa.png",
-          revision: "a2375c352e7aa5b1d12b0d1bad462e77",
+          revision: "42ce7fd40b71b7c51f610c471093d397",
         },
         {
           url: "img/pwa/screenshots/spanish/tablet_10inch_half_life_spa.png",
-          revision: "1b82ab2b208d3d13b56b0e5663aa0dfe",
+          revision: "d1b7a20b1c747886d8f4737efa6d755d",
         },
         {
           url: "img/pwa/screenshots/spanish/tablet_10inch_low_life_spa.png",
-          revision: "db8c858d83fc43a0151920919c82e533",
+          revision: "59a41605a7166eea3a1b5e24b067d030",
         },
         {
           url: "img/pwa/screenshots/spanish/tablet_10inch_start_spa.png",
-          revision: "9228cf5670f0845b04f415a1f85b0939",
+          revision: "3f3aaac5358ab7490a86c742278fcedf",
         },
         {
           url: "img/pwa/screenshots/spanish/tablet_7inch_end_spa.png",
-          revision: "2bc49ee0f8ced5f2f1ea3e9d354715ae",
+          revision: "b8c24932020aa6b1ef21b3033f918947",
         },
         {
           url: "img/pwa/screenshots/spanish/tablet_7inch_half_life_spa.png",
-          revision: "a9bce6bccdd7303ce7cba22a92960cf0",
+          revision: "8b5d51ac730c6c988f8f8159066d834c",
         },
         {
           url: "img/pwa/screenshots/spanish/tablet_7inch_low_life_spa.png",
-          revision: "41baf75485075d3c030df5d4a392d3da",
+          revision: "067636f4cf89596eb3d9cb920f4b4307",
         },
         {
           url: "img/pwa/screenshots/spanish/tablet_7inch_start_spa.png",
-          revision: "56dbb93cdb5c4b197768b1c5b4638108",
+          revision: "cf9e9b9e08f943d4513c335552b11fbf",
         },
-        { url: "index.html", revision: "ba8347cf7218f8c0374947e4566d2c5c" },
-        { url: "js/main.js", revision: "b46cf6fedf401af4710f5274f4c08dd9" },
+        { url: "index.html", revision: "07d9d73c9b08218acdc6c2cca9f72c1c" },
+        { url: "js/main.js", revision: "0062c503b8476467169f243ceb96a783" },
         { url: "js/model.js", revision: "cc6373dc6f269fc94d94ea331ab7b691" },
         { url: "js/utility.js", revision: "7436c90cc6fa9c3b980cd1763514ab6b" },
         {
           url: "json/languages.js",
-          revision: "155d664b1734d8d2952daa9275bba6bf",
+          revision: "a6ce9ee3784c3ae2648597000b05408e",
         },
-        { url: "manifest.json", revision: "4b7614bfb4134e79a3044816f756f33e" },
+        { url: "manifest.json", revision: "8a0f28618bfb4d1c5e3f913baf506220" },
         { url: "README.md", revision: "02fb5aa72241a2455f6299ca6affa612" },
       ],
       { ignoreURLParametersMatching: [/^utm_/, /^fbclid$/] }
